3327d48f73144e1002d4bcffbaeb6d82b8e93bec
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2022-12-15 13:59:11 -0500

    set up dependencies through Maven; tested base DNA/RNA sequences

---
+++ src/main/java/testOpMode.java
@@ -0,0 +1,15 @@
+import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
+import org.biojava.nbio.core.sequence.DNASequence;
+import org.biojava.nbio.core.sequence.RNASequence;
+
+public class testOpMode {
+    DNASequence seq = new DNASequence("GTAC");
+    RNASequence seq2 = new RNASequence("GUAC");
+
+    public testOpMode() throws CompoundNotFoundException {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
