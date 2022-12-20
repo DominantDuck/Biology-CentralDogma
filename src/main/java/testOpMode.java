b0ba13bd49a6fdf471a37ad7e3c45f6280f3250c
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2022-12-20 12:26:34 -0500

    fixed error; improved performance

--- src/main/java/testOpMode.java
+++ src/main/java/testOpMode.java
@@ -2,7 +2,12 @@ import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
 import org.biojava.nbio.core.sequence.DNASequence;
 import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.biojava.nbio.core.sequence.RNASequence;
+import org.biojava.nbio.core.sequence.compound.NucleotideCompound;
+import org.biojava.nbio.core.sequence.template.CompoundSet;
 import org.biojava.nbio.core.sequence.template.SequenceView;
+import org.biojava.nbio.core.sequence.transcription.RNAToAminoAcidTranslator;
+
+import java.util.List;
 import java.util.Scanner;
 
 import javax.sound.midi.Sequence;
@@ -15,13 +20,14 @@ public class testOpMode {
 
 
     public static void main(String[] args) throws CompoundNotFoundException {
-        RNASequence rnaSeq;
+        RNASequence rnaSeqFromInput;
         ProteinSequence proSeq;
 
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter DNA sequence: ");
         String userInput = myObj.nextLine();
+        userInput = userInput.replaceAll("\\s", "");
 
         boolean errorPresent = false;
 
@@ -40,20 +46,88 @@ public class testOpMode {
             System.out.println("Error"); //add more bio vocab describing error
         }
 
-        DNASequence seq = new DNASequence(userInput);
+        DNASequence seq = new DNASequence(userInput.toUpperCase());
 
-        rnaSeq = seq.getRNASequence();
-        String printmRNA = rnaSeq.getSequenceAsString();
+        RNASequence antisenseStrand = seq.getRNASequence();
+
+        String mRNA = antisenseStrand.toString();
+
+        String printmRNA = mRNA;
         printmRNA = printmRNA.toUpperCase();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < printmRNA.length(); i++) {
+            sb.append(printmRNA.charAt(i));
+            if (i % 3 == 2 && i < printmRNA.length() - 1) {
+                sb.append(" ");
+            }
+        }
+
+        printmRNA = sb.toString();
 
         System.out.println("mRNA: " + printmRNA);
 
-        proSeq = rnaSeq.getProteinSequence();
+        SequenceView tRNA = antisenseStrand.getComplement();
+        String tRNAAsString = tRNA.getSequenceAsString();
+        System.out.println("tRNA: " + tRNAAsString);
+
+
+        proSeq = antisenseStrand.getProteinSequence();
         String printPro = proSeq.getSequenceAsString();
         printPro = printPro.toUpperCase();
 
 
-        System.out.println("Protein: " + printPro);
+        String[][] aminoAcids = {
+                {"A", "Ala"},
+                {"R", "Arg"},
+                {"N", "Asn"},
+                {"D", "Asp"},
+                {"C", "Cys"},
+                {"E", "Glu"},
+                {"Q", "Gln"},
+                {"G", "Gly"},
+                {"H", "His"},
+                {"I", "Ile"},
+                {"L", "Leu"},
+                {"K", "Lys"},
+                {"M", "Met"},
+                {"F", "Phe"},
+                {"P", "Pro"},
+                {"S", "Ser"},
+                {"T", "Thr"},
+                {"W", "Trp"},
+                {"Y", "Tyr"},
+                {"V", "Val"}
+        };
+
+        StringBuilder proteinBuilder = new StringBuilder();
+        boolean stopFound = false;
+
+        for (int i = 0; i < printPro.length(); i++) {
+            char singleLetterName = printPro.charAt(i);
+            for (int j = 0; j < aminoAcids.length; j++) {
+                if (aminoAcids[j][0].charAt(0) == singleLetterName) {
+                    proteinBuilder.append(aminoAcids[j][1]);
+                    proteinBuilder.append(" ");
+                    break;
+                }
+                if (printPro.charAt(i) == '*') {
+                    proteinBuilder.append("STOP");
+                    proteinBuilder.append(" ");
+                    stopFound = true;
+                    break;
+                }
+            }
+            if (stopFound == true) {
+                break;
+            }
+        }
+
+        String result = proteinBuilder.toString();
+
+        System.out.println("Amino Acid: " + result);
+
 
 
     }
